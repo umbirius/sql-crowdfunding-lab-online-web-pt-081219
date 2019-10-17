9d853c774f19c42fd9b7a3b160e5c527e47c09b3
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-17 15:33:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.name, pledges.amount 
   FROM pledges JOIN users ON pledges.user_id = users.id
   GROUP BY users.name 
-  ORDER BY SUM(pledges.amount"
+  ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
